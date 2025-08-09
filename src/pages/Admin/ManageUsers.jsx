774c6ef5f960742811928ebb28d236072dcc778b
KanittaJHA <kanittaketin@gmail.com>
2025-08-10 00:12:48 +0700

    Update ManageUsers component (fix typo)

--- src/pages/Admin/ManageUsers.jsx
+++ src/pages/Admin/ManageUsers.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
